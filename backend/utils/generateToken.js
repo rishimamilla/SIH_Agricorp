f4a93ee4bd5476bd66bf93b38ab3302ade1c9eee
Mukesh Chevula <mukeshchevula@gmail.com>
2024-09-05 20:40:24 +0530

    interfaces step 1

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d", // Set token expiration time as needed
-  });
-};
-
-export default generateToken;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const cors = require("cors");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+const app = express();
+app.use(express.json());
+connectDB(); // Connect to databse
+app.use(cors());
+// API Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+// --------------------------DEPLOYMENT------------------------------
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) => {
+    return res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+// --------------------------DEPLOYMENT------------------------------
+
+// Error Handler Middleware (Should be at the end of all middlewares)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server running on PORT ${PORT}`)
+);
+
+// Handling server errors with clean error messages
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
